a20944a7c1ca242eb9511887dcf44b22ae48a638
Eric Kivel <ericvkivel@gmail.com>
2020-07-30 15:11:39 -0300

    Cadastro de "videos" e de "categorias"

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu';
-import BannerMain from './components/BannerMain';
-import dadosIniciais from './data/dados_iniciais.json';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414" }}>
       <Menu />
@@ -41,9 +41,9 @@ function App() {
         category={dadosIniciais.categorias[5]}
       />
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
