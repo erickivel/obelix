36aef230eefee7df9e9ec9e60a080ded7f588655
Eric Kivel <ericvkivel@gmail.com>
2020-07-30 15:51:32 -0300

    Slider (infinite), inicio form categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,22 @@ function CadastroCategoria () {
     return (
         <PageDefault>
             <h1>Cadastro de categoria</h1>
+
+            <form>
+
+                <label>
+                    Nome da Categoria:
+                    <input
+                        type="text"
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+                </button>
+
+            </form>
+
             
             <Link to="/">
                 Ir para home
