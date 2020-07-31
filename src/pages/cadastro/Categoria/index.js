a57cecfccbcb2cc98fcbb113cf5389278e7e03a4
Eric Kivel <ericvkivel@gmail.com>
2020-07-30 21:15:50 -0300

    Inicio - state - catregorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,21 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 
-function CadastroCategoria () {
+function CadastroCategoria() {
+    const [categorias, setCategorias] = useState(['Teste', 'Filmes', 'Marketing']);
+    
+    const valoresIniciais = {
+        nome: 'awdwa',
+        descricao: 'descricva',
+        cor: '#000',
+    }
+    const [values, setValues] = useState(valoresIniciais);
+
+    function setValue(chave, valor) {
+        setValues({
+           ...values,
+           [chave]: valor, 
+        })
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastro de categoria</h1>
+            <h1>Cadastro de categoria: {values.nome}</h1>
 
-            <form>
-
-                <label>
-                    Nome da Categoria:
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    values
+                ]);
+            }}>
+                <div>
+                    <label>
+                        Nome da Categoria:
                     <input
-                        type="text"
-                    />
-                </label>
+                            type="text"
+                            value={values.nome}
+                            onChange={function funcaoHandler(infosDoEvento) {
+                            // setValues(infosDoEvento.target.value);
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Descrição:
+                    <textarea
+                            type="text"
+                            value={values.descricao}
+                            onChange={function funcaoHandler(infosDoEvento) {
+                            // set.values(infosDoEvento.target.value);
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Cor:
+                    <input
+                            type="color"
+                            value={values.cor}
+                            onChange={function funcaoHandler(infosDoEvento) {
+                            // set.values(infosDoEvento.target.value);
+                            }}
+                        />
+                    </label>
+                </div>
+
 
                 <button>
                     Cadastrar
@@ -23,7 +75,17 @@ function CadastroCategoria () {
 
             </form>
 
-            
+            <ul>
+                {categorias.map((categoria, index) => {
+                    return (
+                        <li key={`${categoria}${index}`}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
+
+
             <Link to="/">
                 Ir para home
             </Link>
