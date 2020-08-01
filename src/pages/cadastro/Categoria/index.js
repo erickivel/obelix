e561a91f6f71309e075fb0628381b4973d73a7d8
Eric Kivel <ericvkivel@gmail.com>
2020-08-01 09:17:50 -0300

    Atualização beck-end

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,8 +31,9 @@ function CadastroCategoria() {
 
 
   useEffect(() => {
-    console.log('nawodjefbesf');
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost') 
+      ? 'http://localhost:8080/categorias'
+      : 'https://obelix-kivel.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
